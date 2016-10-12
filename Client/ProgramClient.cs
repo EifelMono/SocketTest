9a142ea707e9a5cd5c2e41d98948314e2851cd9d
Andreas Klapperich <Andreas@Klapperich.de>
2016-10-12 11:47:41 +0200

    intial checckin

---
+++ Client/ProgramClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int Port = 9898;
+                string ServerIp = "127.0.0.1";
+                if (args.Length > 0)
+                {
+                    ServerIp = args[0];
+                }
+                Console.Title = "Client";
+                string textToSend = DateTime.Now.ToString();
+                textToSend = File.ReadAllText("RowaScalaVisuData.txt");
+                TcpClient client = new TcpClient(ServerIp, Port);
+
+                NetworkStream nwStream = client.GetStream();
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+                //---send the text---
+                Console.WriteLine("Sending : " + textToSend.Length);
+                client.SendBufferSize = bytesToSend.Length;
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                nwStream.Flush();
+                // Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+                Console.ReadLine();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Server/ProgramServer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        internal static int ReceiveBufferSize = 4194304; // 4MB Puffer (Default ist 64KB...)
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int Port = 9898;
+                string ServerIp = "127.0.0.1";
+
+                IPAddress ipAddress = IPAddress.Parse(ServerIp);
+                TcpListener listener = new TcpListener(ipAddress, Port);
+                Console.WriteLine("Listening...");
+                listener.Start();
+
+                var mem = new MemoryStream();
+
+                TcpClient client = listener.AcceptTcpClient();
+                client.ReceiveBufferSize = ReceiveBufferSize;
+                int bytesRead = 0;
+                int totalyBytesRead = 0;
+                NetworkStream nwStream = client.GetStream();
+                do
+                {
+                    Thread.Sleep(100); // delay!!!
+                    byte[] buffer = new byte[client.ReceiveBufferSize];
+                    bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+                    totalyBytesRead += bytesRead;
+                    mem.Write(buffer, 0, bytesRead);
+                } while (bytesRead == client.ReceiveBufferSize);
+
+                Console.WriteLine($"Received Bytes: {totalyBytesRead}");
+
+                string dataReceived = "";
+                mem.Position = 0;
+                using (StreamReader reader = new StreamReader(mem, Encoding.UTF8))
+                    dataReceived = reader.ReadToEnd();
+                File.WriteAllText("RowaScalaVisuData.txt", dataReceived);
+                Console.WriteLine($"Received string: {dataReceived.Length}");
+
+                client.Close();
+                listener.Stop();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
